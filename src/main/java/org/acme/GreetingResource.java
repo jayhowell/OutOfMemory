64ffb5ca4290b2379d54ab0e3b972aeb1e779977
Jay Howell <jhowell@redhat.com>
2023-07-03 10:39:07 -0400

    Adding all new files

---
+++ src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,36 @@
+package org.acme;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Path("status")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello again RESTEasy";
+    }
+
+    @GET
+    @Path("oom")
+    @Produces(MediaType.TEXT_PLAIN)
+    public void oom() {
+        List<byte[]> list = new LinkedList<>();
+        int index = 1;
+        
+        while (true) {
+            byte[] b = new byte[10 * 1024 * 1024]; // 10MB byte object
+            list.add(b);
+            Runtime rt = Runtime.getRuntime();
+            System.out.printf("[%3s] Available heap memory: %s%n", index++, rt.freeMemory());
+        }
+            
+    }
+}
